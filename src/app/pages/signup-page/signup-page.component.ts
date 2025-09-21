53ab05d62fbd09c557d8279d4fe6b4b399dd494d
usamaparkar1 <usamaparkar1@gmail.com>
2025-09-21 11:34:36 +0530

    Create a sign up page and import sign up form

---
+++ src/app/pages/signup-page/signup-page.component.ts
@@ -0,0 +1,21 @@
+import { SignupFormComponent } from "../../components/signup-form/signup-form.component";
+import { RoutingService } from "../../services/routing/routing.service";
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-page',
+  templateUrl: './signup-page.component.html',
+  styleUrl: './signup-page.component.scss',
+  imports: [SignupFormComponent]
+})
+
+export class SignupPageComponent {
+
+  constructor(
+    private _routingService: RoutingService
+  ) {}
+
+  onSignupSuccess() {
+    this._routingService.goToLoginPage();
+  }
+}
