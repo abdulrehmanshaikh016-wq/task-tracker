b3c3e0ea3b66a278ec8ae0169840c8a121054b40
usamaparkar1 <usamaparkar1@gmail.com>
2025-09-21 15:01:39 +0530

    Create a sign up form

--- src/app/components/signup-form/signup-form.component.ts
+++ src/app/components/signup-form/signup-form.component.ts
@@ -1,12 +1,99 @@
-import { Component } from '@angular/core';
+import { SignupFormValidatorService } from '../../utils/signup-form-validator/signup-form-validator.service';
+import { SignupFormBuilderService } from '../../utils/signup-form-builder/signup-form-builder.service';
+import { PasswordInputComponent } from "../inputs/password-input/password-input.component";
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RoutingService } from '../../services/routing/routing.service';
+import { SignupService } from '../../services/signup/signup.service';
+import { SignupModel } from '../../models/signup-model';
+import { SignupForm } from '../../forms/signup-form';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup-form',
-  imports: [],
   templateUrl: './signup-form.component.html',
-  styleUrl: './signup-form.component.scss'
+  styleUrl: './signup-form.component.scss',
+  imports: [ReactiveFormsModule, PasswordInputComponent]
 })
 
-export class SignupFormComponent {
+export class SignupFormComponent implements OnInit {
 
+  isSigningUpUser: boolean = false;
+  signupForm: FormGroup<SignupForm>;
+
+  constructor(
+    private _signupFormValidator: SignupFormValidatorService,
+    private _signupFormBuilder: SignupFormBuilderService,
+    private _routingService: RoutingService,
+    private _signupService: SignupService
+  ) {
+    this.signupForm = this._signupFormBuilder.createSignupForm();
+  }
+
+  ngOnInit(): void {
+    this._setupSignupPage();
+  }
+
+  get signupUsernameControl(): FormControl {
+    return this.signupForm.controls.username;
+  }
+
+  get signupPasswordControl(): FormControl {
+    return this.signupForm.controls.password;
+  }
+
+  get signupConfirmPasswordControl(): FormControl {
+    return this.signupForm.controls.confirmPassword;
+  }
+
+  getPasswordError(passwordFormControl: FormControl): string | null {
+    return this._signupFormValidator.getPasswordError(passwordFormControl);
+  }
+
+  getConfirmPasswordError(control: FormControl): string | null {
+    // Check individual password field errors
+    const passwordError = this._signupFormValidator.getPasswordError(control);
+    if (passwordError) return passwordError;
+
+    // Only show mismatch if user has interacted with confirm password
+    const confirmPasswordControl = this.signupConfirmPasswordControl;
+    if ((confirmPasswordControl.touched || confirmPasswordControl.dirty) && this.signupForm.errors?.['passwordMismatch']) {
+      return 'Passwords do not match';
+    }
+
+    return null;
+  }
+
+  private _setupSignupPage(): void {
+  }
+
+  async onSignupSubmit() {
+    if (!this.signupForm.valid) {
+      this.signupForm.markAllAsTouched();
+      this.signupForm.markAllAsDirty();
+      return;
+    }
+
+    // User details are valid. Proceed to login.
+    this._handleUserSignup();
+  }
+
+  private async _handleUserSignup() {
+    const userSignupPayload = this._createPayloadForUserSignup();
+
+    if (!userSignupPayload) return;
+
+    const userSignupResponse = await this._signupService.createNewUser(userSignupPayload);
+
+    if (!userSignupResponse) return;
+
+    this._routingService.goToLoginPage();
+  }
+
+  private _createPayloadForUserSignup() {
+    return new SignupModel({
+      username: this.signupUsernameControl.value,
+      password: this.signupPasswordControl.value,
+      confirmPassword: this.signupConfirmPasswordControl.value,
+    });
+  }
 }
